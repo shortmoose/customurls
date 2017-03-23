0e660fed9163707e171e5ae63cf56b3340c34942
Nathan Bullock <nathanbullock@gmail.com>
2017-03-22 16:00:07 -0800

    easybuild switched to using kingpin

--- cmd/custom-url/main.go
+++ cmd/custom-url/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"os"
+
 	"github.com/nthnca/customurls/config"
 	"github.com/nthnca/easybuild"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
-	easybuild.Build(config.Path, config.ProjectID)
+	app := kingpin.New(
+		"custom-url",
+		"URL shortening service that runs in Google AppEngine")
+	easybuild.RegisterCommands(app, config.Path, config.ProjectID)
+	kingpin.MustParse(app.Parse(os.Args[1:]))
 }
