c9aecc207b6f88cfd007bca19ebdb17d574fa783
Nathan Bullock <nathanbullock@gmail.com>
2017-03-22 16:00:01 -0800

    Completely move to go, no more bash scripts

---
+++ cmd/custom-url/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/nthnca/customurl/config"
+)
+
+func runCmd(command_line string) {
+	args := strings.Split(command_line, " ")
+	log.Printf("Running cmd: %q", args)
+
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Error running command")
+	}
+}
+
+func run() {
+	runCmd("goapp serve -port 8002 -host 192.168.86.2 gaeweb/")
+}
+
+func upload() {
+	runCmd(fmt.Sprintf("goapp deploy -application %s gaeweb/",
+		config.ProjectID))
+}
+
+func watch() {
+	runCmd("go-build")
+}
+
+func main() {
+	var w bool = false
+	flag.BoolVar(&w, "w", false, "")
+	flag.BoolVar(&w, "watch", false, "Watch")
+
+	var r bool = false
+	flag.BoolVar(&r, "r", false, "")
+	flag.BoolVar(&r, "run", false, "Run")
+
+	var u bool = false
+	flag.BoolVar(&u, "u", false, "")
+	flag.BoolVar(&u, "upload", false, "Upload")
+
+	flag.Parse()
+
+	os.Chdir(config.Path)
+	if w {
+		watch()
+	} else if r {
+		run()
+	} else if u {
+		upload()
+	}
+}
