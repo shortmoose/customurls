6cdcef5469ea68f372cc7fe1f2704be61dc0be14
Nathan Bullock <nathanbullock@gmail.com>
2018-05-20 01:15:45 +0000

    Allow modification and listing of shortcuts
    - This code is pretty ugly/repetitive, should be cleaned up shortly.
    - Still need to enforce a bit more authentication/authorization

--- gaeweb/code.go
+++ gaeweb/code.go
@@ -3,88 +3,193 @@ package code
 import (
 	"fmt"
 	"net/http"
+	"regexp"
+	"sort"
 	"strings"
+	"time"
 
 	"github.com/nthnca/customurls/config"
 	"github.com/nthnca/customurls/data/client"
+	"github.com/nthnca/customurls/data/entity"
 
 	"github.com/nthnca/datastore"
-	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 )
 
-func showForm(w http.ResponseWriter) {
+type usage struct {
+	key     string
+	url     string
+	week    int
+	month   int
+	allTime int
+}
+
+func showStats(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	clt := datastore.NewGaeClient(ctx)
+
+	data := make(map[string]usage)
+
+	var entries []entity.Entry
+	keys, _ := clt.GetAll(clt.NewQuery("Entry"), &entries)
+
+	for i := range entries {
+		data[keys[i].GetName()] = usage{
+			key: keys[i].GetName(),
+			url: entries[i].Value}
+	}
+
+	var logs []entity.LogEntry
+	_, err := clt.GetAll(clt.NewQuery("LogEntry"), &logs)
+	if err != nil {
+		log.Warningf(ctx, "Unable to get log entries: %v\n", err)
+		return
+	}
+
+	now := time.Now()
+	week := now.Add(-time.Hour * 24 * 7)
+	month := now.Add(-time.Hour * 24 * 7 * 28)
+
+	for _, log := range logs {
+		e, ok := data[log.Key]
+		if !ok {
+			continue
+		}
+		if log.Timestamp.After(week) {
+			e.week++
+		}
+		if log.Timestamp.After(month) {
+			e.month++
+		}
+		e.allTime++
+		data[log.Key] = e
+	}
+
+	var arr []usage
+	for _, value := range data {
+		arr = append(arr, value)
+	}
+
+	sort.Slice(arr, func(i, j int) bool {
+		if arr[i].week != arr[j].week {
+			return arr[i].week < arr[j].week
+		}
+		if arr[i].month != arr[j].month {
+			return arr[i].month < arr[j].month
+		}
+		return arr[i].allTime < arr[j].allTime
+	})
+
+	for _, y := range arr {
+		fmt.Fprintf(w, "%-15s %4d %4d %4d\n", y.key, y.week, y.month,
+			y.allTime)
+	}
+}
+
+// Returns a lowercase version of the given string, returns an empty string if invalid.
+func getKey(str string) string {
+	if !regexp.MustCompile(`^[A-Za-z0-9-]+$`).MatchString(str) {
+		return ""
+	}
+
+	return strings.ToLower(str)
+}
+
+func redirectAndLog(w http.ResponseWriter, r *http.Request, key string) {
+	ctx := appengine.NewContext(r)
+
+	key = getKey(key)
+	if key == "" {
+		log.Warningf(ctx, "Invalid key attempted.")
+		http.Redirect(w, r, config.DefaultURL, 302)
+		return
+	}
+
+	clt := datastore.NewGaeClient(ctx)
+	entry, err := client.LoadEntry(clt, key)
+	if err != nil {
+		log.Warningf(ctx, "Unable to load '%s': %v", key, err)
+		return
+	}
+
+	log.Infof(ctx, "Redirecting %s to %s", key, entry.Value)
+	client.CreateLogEntry(clt, key, entry.Value)
+	http.Redirect(w, r, entry.Value, 302)
+}
+
+func showUrlModificationForm(w http.ResponseWriter, r *http.Request, key string) {
+	ctx := appengine.NewContext(r)
+	key = getKey(key)
+	if key == "" {
+		log.Warningf(ctx, "Invalid key attempted.")
+		http.Redirect(w, r, config.DefaultURL, 302)
+		return
+	}
+
+	clt := datastore.NewGaeClient(ctx)
+	entry, err := client.LoadEntry(clt, key)
+	if err != nil {
+		log.Warningf(ctx, "Unable to load '%s': %v", key, err)
+		showUrlForm(w, key, "")
+		return
+	}
+
+	showUrlForm(w, key, entry.Value)
+}
+
+func showUrlForm(w http.ResponseWriter, key, url string) {
 	html := `<html>
-<form name="addgolink" action="https://%s.appspot.com">
+<form name="addgolink" action="/" method="post">
 Key:<br />
-<input type="text" name="key"><br />
+<input type="text" name="key" value="%s"><br />
 URL:<br />
-<input type="text" name="url"><br />
+<input type="text" name="url" value="%s"><br />
 Validate:<br />
 <input type="text" name="check"><br />
 <br />
 <input type="submit" value="Submit">
 </form></html>`
-	fmt.Fprintf(w, html, config.ProjectID)
+	fmt.Fprintf(w, html, key, url)
 }
 
-func load(ctx context.Context, key, url string) string {
-	if key == "" {
-		return config.DefaultURL
-	}
-	clt := datastore.NewGaeClient(ctx)
-	entry, err := client.LoadEntry(clt, key)
-	if err != nil {
-		log.Warningf(ctx, "Unable to load '%s': %v", key, err)
-		if len(url) > 4 && url[:4] == "http" {
-			log.Infof(ctx, "Inserting %s:%s", key, url)
-			client.CreateEntry(clt, key, url)
-		}
-		return config.DefaultURL
-	}
-
-	log.Infof(ctx, "Redirecting %s to %s", key, entry.Value)
-	client.CreateLogEntry(clt, key, entry.Value)
-	return entry.Value
-}
-
-func getKey(r *http.Request) string {
-	key := strings.TrimLeft(r.URL.Path, "/")
-	if key != "" {
-		return key
-	}
-
-	if v, ok := r.URL.Query()["key"]; ok && v[0] != "" {
-		return v[0]
-	}
-
-	return ""
-}
-
-func getURL(r *http.Request) string {
+func saveUrl(w http.ResponseWriter, r *http.Request) {
+	// config.Check of "", means readonly system.
 	if config.Check == "" {
-		return ""
+		return
 	}
 
-	if v, ok := r.URL.Query()["check"]; !ok || v[0] != config.Check {
-		return ""
+	if r.FormValue("check") != config.Check {
+		return
 	}
 
-	if v, ok := r.URL.Query()["url"]; ok {
-		return v[0]
+	ctx := appengine.NewContext(r)
+	key := getKey(r.FormValue("key"))
+	if key == "" {
+		log.Warningf(ctx, "Invalid Key %s", key)
+		return
 	}
-	return ""
+
+	url := r.FormValue("url")
+	if len(url) < 4 || url[:4] != "http" {
+		log.Warningf(ctx, "Invalid URL %s", url)
+		return
+	}
+
+	clt := datastore.NewGaeClient(ctx)
+	client.CreateEntry(clt, key, url)
+	return
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	ctx := appengine.NewContext(r)
-	key := strings.ToLower(getKey(r))
-	if key == config.AddPageKey {
-		showForm(w)
+	if r.Method == "POST" {
+		saveUrl(w, r)
+	} else if "/x/ls" == r.URL.Path {
+		showStats(w, r)
+	} else if len(r.URL.Path) > 3 && "/x/" == r.URL.Path[:3] {
+		showUrlModificationForm(w, r, r.URL.Path[3:])
 	} else {
-		url := load(ctx, strings.ToLower(getKey(r)), getURL(r))
-		http.Redirect(w, r, url, 302)
+		redirectAndLog(w, r, r.URL.Path[1:])
 	}
 }
 
