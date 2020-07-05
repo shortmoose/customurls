e5d5fb83a0e80ff016061585e5bf243ddf8a1617
Nathan Bullock <nathanbullock@gmail.com>
2020-07-05 16:58:35 -0700

    Oops, fix broken push

---
+++ internal/util/util.go
@@ -0,0 +1,15 @@
+package util
+
+import (
+	"regexp"
+	"strings"
+)
+
+// Returns a lowercase version of the given string, returns an empty string if invalid.
+func GetKey(str string) string {
+	if !regexp.MustCompile(`^[A-Za-z0-9-]+$`).MatchString(str) {
+		return ""
+	}
+
+	return strings.ToLower(str)
+}
